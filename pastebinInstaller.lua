1a71981bff5f4bb264873f93e55b0beb439280e8
HeroBrine1st <herobrine1.videos@gmail.com>
2019-01-13 17:04:07 +0300

    Delete pastebinInstaller.lua

--- pastebinInstaller.lua
+++
@@ -1,4 +0,0 @@
-print("Downloading installer...")
-os.execute("wget https://raw.githubusercontent.com/HeroBrine1st/UniversalInstaller/master/installer.lua /tmp/installer.lua -f -q")
-print("Running:")
-os.execute("/tmp/installer.lua")
